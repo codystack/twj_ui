adc98070bdc8194998872924903a94c7cc57d773
Tnath1 <aromejonathanu@gmail.com>
2025-03-04 14:51:20 +0100

    profile is done

--- src/pages/Logged_in/Profile.tsx
+++ src/pages/Logged_in/Profile.tsx
@@ -9,6 +9,7 @@ import BankIcon from "../../assets/dashboard_img/profile/Bank_icon.svg";
 import AddRing from "../../assets/dashboard_img/profile/Add_ring_light.svg";
 import Cancel from "../../assets/dashboard_img/profile/cancel.svg";
 import BgImage from "../../assets/dashboard_img/profile/atmcard.jpg";
+import eye from "../../assets/auth_imgs/Eye_light.svg";
 import { useState } from "react";
 import ToggleButton from "../../components/ToggleButton";
 import { NavLink } from "react-router";
@@ -18,13 +19,85 @@ const Profile = () => {
     "account"
   );
   const [showForm, setShowForm] = useState(false);
-  const [reason, setReason] = useState("");
+  // const [reason, setReason] = useState("");
+  const [errors, setErrors] = useState({
+    reason: "",
+    password: "",
+  });
+  const [formData, setFormData] = useState({
+    reason: "",
+    password: "",
+  });
+  // State for password visibility
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  // Update form field value
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    validateField(name, value);
+  };
+
+  // Toggle password visibility
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const validateField = (fieldName: string, value: string) => {
+    switch (fieldName) {
+      case "reason":
+        // const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!value.trim()) {
+          setErrors((prev) => ({
+            ...prev,
+            reason: "This field is required",
+          }));
+        } else {
+          setErrors((prev) => ({
+            ...prev,
+            reason: "",
+          }));
+        }
+        break;
+
+      case "password":
+        if (value.length === 0) {
+          setErrors((prev) => ({
+            ...prev,
+            password: "This field is required",
+          }));
+        } else if (value.length < 8) {
+          setErrors((prev) => ({
+            ...prev,
+            password: "Please enter a valid password",
+          }));
+        } else {
+          setErrors((prev) => ({
+            ...prev,
+            password: "",
+          }));
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const isFormInvalid =
+    Object.values(errors).some((error) => error) ||
+    !formData.reason ||
+    !formData.password;
 
   return (
     <div className="w-full overflow-hidden h-[calc(100vh-5.2rem)] mr-[2rem] mt-[5rem] rounded-tl-[30px] bg-[#fff] flex flex-col">
       <div className="flex-1 overflow-y-auto p-4">
         {/* Nav */}
-        <div className=" flex flex-col mt-[1.3%] ml-[1%] ">
+        <div className=" flex flex-col mt-[2.3%] ml-[4%] ">
           {/* Tab Buttons */}
           <div className="bg-[#F5F7FA] w-[35%] flex items-center rounded-[50px] justify-between p-[7px]">
             <button
@@ -91,18 +164,18 @@ const Profile = () => {
                     </NavLink>
                   </div>
                 </div>
-                <div className="mt-[3%] px-[0.6rem]">
-                  <div className="flex items-center justify-between mb-[4%] ">
+                <div className="mt-[8%] px-[0.6rem]">
+                  <div className="flex items-center justify-between mb-[6%] ">
                     <p className="text-[#7688B4] text-[14px] ">Unique ID</p>
                     <p className="text-[#27014F] text-[14px]  ">TWJ62425</p>
                   </div>
-                  <div className="flex items-center justify-between mb-[4%] ">
+                  <div className="flex items-center justify-between mb-[6%] ">
                     <p className="text-[#7688B4] text-[14px] ">Email address</p>
                     <p className="text-[#27014F] text-[14px]  ">
                       aromej@gmail.com
                     </p>
                   </div>
-                  <div className="flex items-center justify-between mb-[4%] ">
+                  <div className="flex items-center justify-between mb-[6%] ">
                     <p className="text-[#7688B4] text-[14px]  ">Phone</p>
                     <span className="flex gap-[2px]">
                       <p className="text-[#27014F] text-[14px]  ">
@@ -111,7 +184,7 @@ const Profile = () => {
                       <img src={Edit} alt="" className=" cursor-pointer" />
                     </span>
                   </div>
-                  <div className="flex items-center justify-between mb-[4%] ">
+                  <div className="flex items-center justify-between mb-[7%] ">
                     <p className="text-[#7688B4] text-[14px]  ">
                       Date of birth
                     </p>
@@ -121,7 +194,7 @@ const Profile = () => {
                   </div>
                   <NavLink
                     to="/profile/account_upgrade"
-                    className="flex cursor-pointer items-center w-full justify-between bg-[#FF3366]/13 hover:bg-[#FF3366]/20 transition-colors duration-300 p-[5px] py-[8px] rounded-[10px] border border-[#FF3366] mb-[7%] "
+                    className="flex cursor-pointer items-center w-full justify-between bg-[#FF3366]/13 hover:bg-[#FF3366]/20 transition-colors duration-300 p-[5px] py-[8px] rounded-[10px] border border-[#FF3366] mb-[12%] "
                   >
                     <span className="flex items-center">
                       <span className="bg-[#FF3366]/20 rounded-[100%] p-[2px] mr-[2px] ">
@@ -143,7 +216,7 @@ const Profile = () => {
                     {!showForm ? (
                       <button
                         onClick={() => setShowForm(true)} // Fix: Make sure the modal opens
-                        className="mt-[5%] flex text-[#F80004] items-center gap-2 cursor-pointer "
+                        className="pb-[10%] flex text-[#F80004] items-center gap-2 cursor-pointer "
                       >
                         <span>
                           <img src={Delete} alt="" />
@@ -160,30 +233,110 @@ const Profile = () => {
                               <h3 className="text-[17px] font-semibold text-[#27014F] ">
                                 Delete my TWJ Account
                               </h3>
-                              <button className="cursor-pointer" onClick={() => setShowForm(false)}>
-                                
+                              <button
+                                className="cursor-pointer"
+                                onClick={() => setShowForm(false)}
+                              >
                                 <img src={Cancel} alt="" />
                               </button>
+                            </div>
+                            <div className="flex justify-center my-[5%]">
+                              <span className="bg-[#FF3366]/15 rounded-[100%] w-[5rem] h-[5rem] flex justify-center items-center p-[2px] mr-[2px] ">
+                                <img
+                                  src={alarmIcon}
+                                  className="w-[3.5rem] "
+                                  alt=""
+                                />
+                              </span>
                             </div>
 
                             {/* Textarea */}
                             <textarea
-                              value={reason}
-                              onChange={(e) => setReason(e.target.value)}
-                              className="w-full border p-2 rounded-md resize-none h-24 mt-3"
-                              placeholder="Enter your reason..."
+                              onBlur={() =>
+                                validateField("reason", formData.reason)
+                              }
+                              name="reason"
+                              value={formData.reason}
+                              onChange={handleInputChange} // className=""
+                              className={` w-full border border-[#A4A4A4] p-2  resize-none h-[40%] mt-3 focus:border-2 outline-none rounded-md ${
+                                errors.reason
+                                  ? "border border-red-600"
+                                  : "focus:border-purple-800"
+                              } `}
+                              placeholder="Reason for Deletion..."
                             ></textarea>
+                            {errors.reason && (
+                              <p className="text-red-500 text-xs mt-1">
+                                {errors.reason}
+                              </p>
+                            )}
+
+                            <div className="relative w-full mt-[5%]">
+                              <input
+                                type={isPasswordVisible ? "text" : "password"}
+                                placeholder="Password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleInputChange}
+                                onBlur={() =>
+                                  validateField("password", formData.password)
+                                }
+                                className={`p-2.5 pl-3 pr-3 border text-[13px] border-[#A4A4A4] w-full focus:border-2 outline-none rounded-md ${
+                                  errors.password
+                                    ? "border border-red-600"
+                                    : "focus:border-purple-800"
+                                } `}
+                              />
+                              <img
+                                className="absolute cursor-pointer right-[0.8rem] bottom-[0.45rem]"
+                                src={eye}
+                                alt="password visibility toggle"
+                                onClick={togglePasswordVisibility}
+                              />
+                            </div>
+                            {errors.password && (
+                              <p className="text-red-500 text-xs mt-1">
+                                {errors.password}
+                              </p>
+                            )}
 
                             {/* Buttons */}
-                            <div className="flex justify-between mt-4">
+                            <div className="flex justify-between mt-[5%] w-full ">
                               <button
-                                onClick={() => {
-                                  console.log("Reason:", reason);
-                                  // Handle delete logic here
-                                }}
-                                className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 transition duration-300"
+                                className={`bg-[#9605C5] w-full  text-white p-3 rounded-[6px]  ${
+                                  isFormInvalid
+                                    ? "opacity-60 cursor-not-allowed"
+                                    : "  cursor-pointer"
+                                }`}
+                                disabled={isFormInvalid}
                               >
-                                Confirm Delete
+                                {/* {isLoadingLogin ? (
+                  <div className="flex items-center justify-center">
+                    <svg
+                      className="animate-spin h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                      ></path>
+                    </svg>
+                  </div>
+                ) : (
+                  "Log In"
+                )} */}
+                                Delete Account
                               </button>
                             </div>
                           </div>
@@ -209,7 +362,7 @@ const Profile = () => {
                     Change Password
                   </button>
                 </div>
-                <div className="flex items-center mt-[6%] justify-between leading-[1.2rem]">
+                <div className="flex items-center mt-[10%] justify-between leading-[1.2rem]">
                   <div className=" flex flex-col">
                     <p className="font-[500]">Update PIN</p>
                     <p className="text-[12px] text-[#7688B4]">
@@ -220,10 +373,10 @@ const Profile = () => {
                     Change Pin
                   </button>
                 </div>
-                <div className="flex items-center mt-[6%] justify-between leading-[1.2rem]">
+                <div className="flex items-center mt-[10%] justify-between leading-[1.2rem]">
                   <div className=" flex flex-col">
                     <p className="font-[500]">Two-Factor Authentication</p>
-                    <p className="text-[12px] text-[#7688B4]">
+                    <p className="text-[12px]  text-[#7688B4]">
                       Protect your TWJ account from unauthorised <br />{" "}
                       transaction using a software token
                     </p>
