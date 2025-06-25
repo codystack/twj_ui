2589b926bb0d1346c3e6f3c40ee8fe6834d85995
Tnath1 <aromejonathanu@gmail.com>
2025-06-25 01:51:28 +0100

    help responsiveness is done

--- src/pages/Logged_in/Help.tsx
+++ src/pages/Logged_in/Help.tsx
@@ -10,6 +10,7 @@ import {
 import { AiTwotoneMail } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoIosArrowForward } from "react-icons/io";
+import { GoArrowLeft } from "react-icons/go";
 // const contentMap: Record<string, string> = {
 //   button1: "This is content for Button 1",
 //   button2: "This is content for Button 2",
@@ -76,13 +77,15 @@ const socialMediaButtons = [
 
 const Help = () => {
   const [activeButton, setActiveButton] = useState("button1");
+  // const [activeButtonMobile, setActiveButtonMobile] = useState("");
+  const [showContactModal, setShowContactModal] = useState(false);
 
   return (
     <div className="w-full overflow-hidden h-[calc(100vh-5.2rem)] mr-[2rem] mt-[5rem] rounded-tl-[30px] bg-[#fff] text-center flex flex-col">
       <div className="flex-1 overflow-y-auto p-4">
-        <div className="grid grid-cols-3   gap-8 h-full p-4">
+        <div className="grid grid-cols-3   gap-8 h-full p-4 sm:px-4 px-1">
           {/* Button Column: col-span-1 */}
-          <div className="col-span-1 flex flex-col gap-7">
+          <div className="[@media(min-width:850px)]:flex col-span-1  hidden flex-col gap-7">
             {buttons.map((btn) =>
               btn.type === "link" ? (
                 <a
@@ -116,8 +119,45 @@ const Help = () => {
             )}
           </div>
 
+          {/* Mobile Button Column: col-span-1 */}
+          <div className="[@media(min-width:850px)]:hidden flex col-span-4  flex-col sm:gap-7 gap-5">
+            {buttons.map((btn) =>
+              btn.type === "link" ? (
+                <a
+                  key={btn.id}
+                  href={btn.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="py-[1.5rem] cursor-pointer rounded-[5px] border text-[#27014F] flex items-center sm:gap-2 gap-1 border-[#D0DAE6] hover:border-[#8003A9]"
+                >
+                  <span className="text-xl   pl-[1.25rem] pr-[0.5rem]">
+                    {btn.icon}
+                  </span>
+                  <span>{btn.label}</span>
+                </a>
+              ) : (
+                <button
+                  key={btn.id}
+                  onClick={() => {
+                    setActiveButton(btn.id);
+                    setShowContactModal(true); // show modal
+                  }} // style={{
+                  //   borderColor:
+                  //     activeButton === btn.id ? "#8003A9" : "#A4A4A4",
+                  // }}
+                  className="py-[1.5rem] cursor-pointer rounded-[5px] border border-[#D0DAE6]  hover:border-[#8003A9] text-[#27014F] flex items-center gap-2"
+                >
+                  <span className="text-xl  pl-[1.25rem] pr-[0.5rem]">
+                    {btn.icon}
+                  </span>
+                  <span>{btn.label}</span>
+                </button>
+              )
+            )}
+          </div>
+
           {/* Display Area: spans 3 columns */}
-          <div className="col-span-2 border border-[#D0DAE6] px-8 py-10 rounded-[10px]">
+          <div className="col-span-2 border [@media(min-width:850px)]:block hidden border-[#D0DAE6] px-8 py-10 rounded-[10px]">
             <h2 className="text-[29px] font-semibold mb-2 text-left">
               Contact Us
             </h2>
@@ -151,6 +191,55 @@ const Help = () => {
               </div>
             </div>
           </div>
+          {/* Mobile Display Area: spans 3 columns */}
+          {showContactModal && (
+            <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center ">
+              <div className="bg-white w-full  p-6 h-[min(100dvh,100vh)] max-h-screen shadow-lg relative">
+                <div className="w-full  flex ">
+                  <button
+                    className=" text-left   cursor-pointer mt-[0.6rem] text-gray-600 text-lg font-bold"
+                    onClick={() => setShowContactModal(false)}
+                  >
+                    <GoArrowLeft className="text-[1.5rem]" />
+                  </button>
+                </div>
+
+                <h2 className="sm:text-[29px] text-[22px]  font-semibold sm:mb-2 mb-1 text-left">
+                  Contact Us
+                </h2>
+                <div className="text-[16px] text-[#000] text-left">
+                  <p className="text-[#7688B4]">
+                    You can reach us via the following channels
+                  </p>
+
+                  <div className="flex flex-col  gap-4 mt-4">
+                    {socialMediaButtons.map((social) => (
+                      <a
+                        key={social.name}
+                        href={social.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="py-[1rem] cursor-pointer rounded-[5px] justify-between text-[#27014F] flex items-center gap-2 border-b border-b-[#E2E8F0] last:border-b-0 hover:bg-[#A4A4A4]/10"
+                      >
+                        <div className="flex items-center gap-2">
+                          <span className="text-xl sm:pl-[2rem] pl-[0.8rem] pr-[1.25rem] ">
+                            {social.icon}
+                          </span>
+                          <div className="flex flex-col">
+                            <span className="text-[#717684]">
+                              {social.label}
+                            </span>
+                            <span>{social.name}</span>
+                          </div>
+                        </div>
+                        <span className="pr-[1.25rem]">{social.rightIcon}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
