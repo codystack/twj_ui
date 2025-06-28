a620994a373b1e0150084b0767e13767e1d12569
Tnath1 <aromejonathanu@gmail.com>
2025-06-28 03:54:13 +0100

    wrote a new code to split page title where neccesary

--- src/pages/Logged_in/DashboardLayoutt.tsx
+++ src/pages/Logged_in/DashboardLayoutt.tsx
@@ -68,13 +68,14 @@ const DashboardLayoutt = () => {
   const navigate = useNavigate();
   const logout = useAuthStore((state) => state.logout);
 
-  const location = useLocation(); // Get current path
+  const location = useLocation();
+  const rawPageName = location.pathname.split("/").pop() || "";
 
-  const locations = useLocation();
-  const pageName = locations.pathname
-    .split("/")
-    .pop()
-    ?.replace(/_/g, " ")
+  // Capitalize and insert space before 'crypto' if present
+  const pageName = rawPageName
+    .replace(/_/g, " ")
+    .replace(/crypto$/i, " Crypto")
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
     .toUpperCase();
 
   let CurrentPage;
@@ -85,13 +86,13 @@ const DashboardLayoutt = () => {
     case "/wallet":
       CurrentPage = <Wallet />;
       break;
-      case "/wallet/buycrypto":
+    case "/wallet/buycrypto":
       CurrentPage = <BuyCrypto />;
       break;
-      case "/wallet/sellcrypto":
+    case "/wallet/sellcrypto":
       CurrentPage = <SellCrypto />;
       break;
-      case "/wallet/swapcrypto":
+    case "/wallet/swapcrypto":
       CurrentPage = <SwapCrypto />;
       break;
     case "/transactions":
@@ -166,7 +167,7 @@ const DashboardLayoutt = () => {
             ></div>
           </button>
 
-          <h3 className="text-[#0A2E65] md:text-[16px] text-[14px] md:tracking-[2.5px] tracking-[2px] font-semibold">
+          <h3 className="text-[#0A2E65] md:text-[16px] text-[14px] md:tracking-[2px] tracking-[2px] font-semibold">
             {pageName}
           </h3>
           <div className="flex items-center ">
@@ -484,22 +485,23 @@ const DashboardLayoutt = () => {
 
               <ul className="mb-[15%] flex flex-col gap-2">
                 <li className="flex items-center  ">
-                  <NavLink to="/support"
-                     className={({ isActive }) =>
+                  <NavLink
+                    to="/support"
+                    className={({ isActive }) =>
                       `flex items-center w-[68%] gap-2 transition-colors rounded-r-[50px] duration-100 py-[12px]  pl-[1.7rem] ${
                         isActive
                           ? "white bg-[#8003A9] rounded-r-[50px] pr-[30px] text-[#fff]"
                           : "text-[#27014F]"
                       }`
-                    }>
-                      
+                    }
+                  >
                     <>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
                         height="24"
                         viewBox="0 0 24 24"
-                        fill="currentColor"  
+                        fill="currentColor"
                       >
                         <path
                           d="M19.3259 5.77772C20 6.78661 20 8.19108 20 11C20 13.8089 20 15.2134 19.3259 16.2223C19.034 16.659 18.659 17.034 18.2223 17.3259C17.3409 17.9148 16.1577 17.9892 14 17.9986V18L12.8944 20.2111C12.5259 20.9482 11.4741 20.9482 11.1056 20.2111L10 18V17.9986C7.8423 17.9892 6.65907 17.9148 5.77772 17.3259C5.34096 17.034 4.96596 16.659 4.67412 16.2223C4 15.2134 4 13.8089 4 11C4 8.19108 4 6.78661 4.67412 5.77772C4.96596 5.34096 5.34096 4.96596 5.77772 4.67412C6.78661 4 8.19108 4 11 4H13C15.8089 4 17.2134 4 18.2223 4.67412C18.659 4.96596 19.034 5.34096 19.3259 5.77772Z"
@@ -527,8 +529,7 @@ const DashboardLayoutt = () => {
                       </svg>
                       Help
                     </>
-                
-                 </NavLink>
+                  </NavLink>
                 </li>
                 <li className="flex  items-center gap-2">
                   <div className="relative">
